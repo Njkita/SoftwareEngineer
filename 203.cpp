bcf8acaa437e0a76e2ec5ce4ce85a0f9b6204239
Njkita <117151719+Njkita@users.noreply.github.com>
2024-09-24 02:42:55 +0300

    Update 203.cpp

--- 203.cpp
+++ 203.cpp
@@ -3,58 +3,42 @@
 #include <string>
 #include <cmath> 
 #include <map>
+#include <iterator>
+#include <algorithm>
 
-
-std::vector<size_t> Divisor(size_t N) {
-    std::vector<size_t> divisor_v;
-    for (size_t i = 1; i <= std::sqrt(N); ++i) {
-        if (N % i == 0) {
-            divisor_v.push_back(i);
-            if (i != N / i) {
-                divisor_v.push_back(N / i);
-            }
-        }
+std::vector<size_t> Catch(std::vector<size_t> v, size_t n) {
+    if (std::find(v.begin(), v.end(), n) == v.end()) {
+        v.push_back(n);
     }
-    return divisor_v;
+    return v;
 }
 
-bool Prime(size_t n) {
-    if (n == 2 || n == 3) {
-        return true;
+std::vector<size_t> prime_factors(size_t n) {
+    std::vector<size_t> factors;
+    while (n % 2 == 0) {
+        factors = Catch(factors, 2);
+        
+        n = n / 2;
     }
-    if ((n == 1) || (n%2  == 0) || (n%3 == 0)) {
-        return false;
-    }
-    
-    for (size_t i = 5; i <= std::sqrt(n); i += 6) {
-        if ((n % i == 0) || (n % (i + 2) == 0)) {
-            return false;
+
+    for (size_t i = 3; i <= std::sqrt(n); i += 2) {
+        while (n%i == 0) {
+            factors = Catch(factors, i);
+            n = n / i;
         }
     }
-    return true;
-}    
 
+    if (n > 2) {
+        factors = Catch(factors, n);
+    }
+    return factors;
+}
 
-
-int main() {
-    size_t N;
-    std::cin >> N;
-    
-    std::vector<size_t> v = Divisor(N);
-    std::vector<size_t> V;
-    
-    for (short y = 0; y < v.size(); ++y) {
-        if (Prime(v[y]) ) {
-            V.push_back(v[y]);
-        }
-    }
-    for (short i = 0; i < v.size(); ++i) {
-        std::cout << v[i] << " ";
-    }
-    std::cout << '\n';
-    for (short i = 0; i < V.size(); ++i) {
-        std::cout << V[i] << " ";
-    }
-    
-    return 0;
+int main()
+{
+    size_t number = 0;
+    std::cin >> number;
+    auto factors = prime_factors(number);
+    std::copy(std::begin(factors), std::end(factors),
+    std::ostream_iterator<size_t>(std::cout, " "));
 }
