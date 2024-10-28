9b418cac3d961f3c6398bee4dcfeb8f24b0939e1
Njkita <117151719+Njkita@users.noreply.github.com>
2024-10-29 01:09:28 +0300

    Update 404_1.cpp

--- 404_1.cpp
+++ 404_1.cpp
@@ -5,21 +5,21 @@ int sum_() {
     return 0;
 }
 
+int get_int(int value) {
+    return value;
+}
+
 template <typename T>
 int get_int(T value) {
-    if constexpr (std::is_same_v<T, int>) {
-        return value;
-    } else {
-        return 0;
-    }
+    return 0;
 }
 
 template <typename T, typename... Ts> 
 int sum_(T first, Ts... next) {
-    return get_int(first) + sum_(next...);  // Суммируем только целые
+    return get_int(first) + sum_(next...);
 }
 
 int main() {
     std::cout << sum_(0, 2.54, 13, std::string("string"), 7, 0.25, 'a') << std::endl;
     return 0;
-}    
+}  
