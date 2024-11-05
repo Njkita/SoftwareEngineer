c56fca6232ad78d37cbf958ffdb3b34517139d82
Njkita <117151719+Njkita@users.noreply.github.com>
2024-11-06 02:48:19 +0300

    Update 413.cpp

--- 413.cpp
+++ 413.cpp
@@ -1,30 +1,30 @@
 #include <type_traits>
 
-template <typename T>
-struct decay {
-    using type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+template <typename T> struct decay {
+private:
+  using U = typename std::remove_reference<T>::type;
+
+public:
+  using type = typename std::conditional<
+      std::is_array<U>::value,
+      typename std::add_pointer<typename std::remove_extent<U>::type>::type,
+      typename std::conditional<std::is_function<U>::value,
+                                typename std::add_pointer<U>::type,
+                                typename std::remove_cv<U>::type>::type>::type;
 };
 
-template <typename T, std::size_t n>
-struct decay<T[n]> {
-    using type = T*;
-};
+template <typename T> using my_decay_t = typename decay<T>::type;
 
-template <typename T, typename... Ts>
-struct decay<T(Ts...)> {
-    using type = T(*)(Ts...);
-};
-
-template <typename T, typename... Ts>
-struct decay<T(Ts..., ...)> {
-    using type = T(*)(Ts..., ...);
-};
+template <typename T, typename U>
+constexpr bool is_decay_equ = std::is_same<my_decay_t<T>, U>::value;
 
 int main() {
-    static_assert(std::is_same<decay<int>::type, int>::value);
-    static_assert(std::is_same<decay<const int&>::type, int>::value);
-    static_assert(std::is_same<decay<int[3]>::type, int*>::value);
-    static_assert(std::is_same<decay<int(int)>::type, int(*)(int)>::value);
-
-    return 0;
+  static_assert(is_decay_equ<int, int> && !is_decay_equ<int, float> &&
+                is_decay_equ<int &, int> && is_decay_equ<int &&, int> &&
+                is_decay_equ<const int &, int> && is_decay_equ<int[2], int *> &&
+                !is_decay_equ<int[4][2], int *> &&
+                !is_decay_equ<int[4][2], int **> &&
+                is_decay_equ<int[4][2], int(*)[2]> &&
+                is_decay_equ<int(int), int (*)(int)>);
+  return 0;
 }
