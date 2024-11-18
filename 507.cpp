9017c8a4b5f52455597e790e058c640ba498cc3d
Njkita <117151719+Njkita@users.noreply.github.com>
2024-11-18 20:47:11 +0300

    Update 507.cpp

--- 507.cpp
+++ 507.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 
-
-struct Client {
-  void test() const { std::clog << "Client::test\n"; }
-};
-
 struct Entity {
   virtual ~Entity() = default;
   virtual void test() const = 0;
 };
 
-class Adapter : private Client, public Entity {
+struct Client1 {
+  void test() const { std::clog << "Client1::test\n"; }
+};
+
+struct Client2 {
+  void test() const { std::clog << "Client2::test\n"; }
+};
+
+template <typename ClientType>
+class Adapter : public Entity {
+private:
+  ClientType client;
 public:
-  void test() const override { Client::test(); }
+  void test() const override { client.test(); }
 };
 
 int main() {
-  Entity *entity = new Adapter();
-  entity->test();
-
-  delete entity;
+  Entity *entity1 = new Adapter<Client1>();
+  entity1->test();
+  Entity *entity2 = new Adapter<Client2>();
+  entity2->test();
+  delete entity1;
+  return 0;
 }
