c703ff21918b772d3e1542ee1a45fc2227857374
Njkita <117151719+Njkita@users.noreply.github.com>
2024-11-18 20:46:22 +0300

    Update 414.cpp

--- 414.cpp
+++ 414.cpp
@@ -4,12 +4,15 @@ template <typename T, typename = std::void_t<>>
 struct is_class : std::false_type {};
 
 template <typename T>
-struct is_class<T, std::void_t<void (T::*)()>> : std::true_type {};
+struct is_class<T, std::void_t<void (T::*)()>>
+    : std::bool_constant<!std::is_union_v<T>> {};
 
 class Class {};
+union MyUnion {};
 
 int main() {
   static_assert(is_class<Class>::value, "Class is class");
   static_assert(!is_class<int>::value, "int isn't class");
+  static_assert(!is_class<MyUnion>::value, "MyUnion isn't class");
   return 0;
 }
