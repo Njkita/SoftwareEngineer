a3ce4ecc496434c0ff393d2fe42d61a2463c8714
Njkita <117151719+Njkita@users.noreply.github.com>
2025-03-08 19:43:38 +0300

    Update 908.cpp

--- 09week/908.cpp
+++ 09week/908.cpp
@@ -1,33 +1,56 @@
 #include <iostream>
 #include <memory>
 
+namespace DataNamespace {
 struct Data {
-    int a;
-    double b;
-    std::string text;
+  int a;
+  double b;
+  std::string text;
+
+  ~Data() { std::cout << "[~Data]\n"; }
 };
+}
+
+namespace ContainerNamespace {
+using namespace DataNamespace;
 
 struct Container {
-    std::shared_ptr<Data> data;
+  std::shared_ptr<Data> data;
 
-    explicit Container(int x, double y, std::string z)
-        : data(std::make_shared<Data>(Data{x, y, std::move(z)})) {}
+  explicit Container(int x, double y, std::string z)
+      : data(std::make_shared<Data>(Data{x, y, std::move(z)})) {}
 
-    std::shared_ptr<int> getA() { return std::shared_ptr<int>(data, &data->a); }
-    std::shared_ptr<double> getB() { return std::shared_ptr<double>(data, &data->b); }
-    std::shared_ptr<std::string> getText() { return std::shared_ptr<std::string>(data, &data->text); }
+  std::shared_ptr<int> getA() { return {data, &data->a}; }
+  std::shared_ptr<double> getB() { return {data, &data->b}; }
+  std::shared_ptr<std::string> getText() { return {data, &data->text}; }
 };
+}
 
 int main() {
-    Container c(42, 3.14, "Пример");
+  using namespace ContainerNamespace;
 
-    std::shared_ptr<int> pA = c.getA();
-    std::shared_ptr<double> pB = c.getB();
-    std::shared_ptr<std::string> pText = c.getText();
+  std::cout << "Создаём контейнер\n";
+  auto c = std::make_unique<Container>(42, 3.14, "Пример");
+
+  {
+    std::cout << "Получаем указатели\n";
+    auto pA = c->getA();
+    auto pB = c->getB();
+    auto pText = c->getText();
 
     std::cout << "a: " << *pA << "\n";
     std::cout << "b: " << *pB << "\n";
     std::cout << "text: " << *pText << "\n";
+    std::cout << "Выходим из области видимости\n";
+  }
 
-    return 0;
+  {
+    std::cout << "Новый shared_ptr на объект\n";
+    auto pNew = c->data;
+  }
+
+  std::cout << "Уничтожаем контейнер\n";
+  c.reset();
+  std::cout << "Конец\n";
+  return 0;
 }
