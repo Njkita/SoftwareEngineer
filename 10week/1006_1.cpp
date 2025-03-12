7ed7e4bc32f8a9701f3b3dcf4a61eeb9b2f05c26
Njkita <117151719+Njkita@users.noreply.github.com>
2025-03-12 22:34:21 +0300

    Update 1006_1.cpp

--- 10week/1006_1.cpp
+++ 10week/1006_1.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <map>
 
-// Грубо меняем память через reinterpret_cast
+struct NodeBase {
+  void *parent;
+  void *left;
+  void *right;
+};
 
+template <typename Key, typename Value> struct Node : NodeBase {
+  std::pair<const Key, Value> data;
+};
+
+// Получаем доступ к данным, которые хранит контейнер, как к полям класса
 int main() {
   std::map<int, std::string> myMap = {{1, "One"}, {2, "Two"}, {3, "Three"}};
 
   auto it = myMap.find(2);
+  // Получаем указатель на const ключ
   if (it != myMap.end()) {
-    int *keyPtr = reinterpret_cast<int *>(
-        const_cast<void *>(static_cast<const void *>(&it->first)));
+    Node<int, std::string> *node = reinterpret_cast<Node<int, std::string> *>(
+        const_cast<void *>(reinterpret_cast<const void *>(&(*it))));
+    int *keyPtr = const_cast<int *>(&node->data.first);
     *keyPtr = 10;
   }
 
